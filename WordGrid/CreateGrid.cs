27495322dfa1dacc2f3b0b2e162c80f29b2b55ec
Ben Gordon <ben.rjg@outlook.com>
2022-05-16 00:23:08 +0100

    Updated outputs

--- WordGrid/CreateGrid.cs
+++ WordGrid/CreateGrid.cs
@@ -5,14 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 
-namespace ConsoleApp1
+namespace WordGrid
 {
     class CreateGrid
     {
+        #region Variables
+        private static string FILEPATH = "../../../enable1.txt";
+
         private int _gridSize;
         private string _characters;
         private List<string> _wordList = new List<string>();
         private List<string> _wordGrid = new List<string>();
+        private TimeSpan _timeCompleted;
+        #endregion
+
 
         public CreateGrid(int gridSize, string characters)
         {
@@ -21,25 +27,18 @@ namespace ConsoleApp1
             List<char> chars = characters.ToList();
             chars.Sort();
             _characters = new string(chars.ToArray());
-
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            start();
-            sw.Stop();
-
-            Console.WriteLine("Final Word Grid found in {0} minutes, {1} seconds, {2} miliseconds:", sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds);
-            foreach (string word in _wordGrid)
-            {
-
-                Console.WriteLine(word);
-            }
         }
 
-        private void start()
+        #region public members
+        public void GenerateGrid()
         {
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
             //Extract valid words from word list
+            Console.WriteLine("Extracting valid words from {0}", FILEPATH);
             int total = 0;
-            foreach (string line in File.ReadLines(@"../../../enable1.txt", Encoding.UTF8))
+            foreach (string line in File.ReadLines(@FILEPATH, Encoding.UTF8))
             {
                 if (line.Length == _gridSize)
                 {
@@ -48,26 +47,26 @@ namespace ConsoleApp1
                         _wordList.Add(line);
                     }
                 }
-
                 total++;
             }
+            Console.WriteLine("{0} valid words out of {1}", _wordList.Count, total);
 
-            /*Console.WriteLine("Available words ({0} out of {1})", _wordList.Count, total);
-            foreach (string word in _wordList)
-            {
-                Console.WriteLine(word);
-            }*/
-
+            //Initialise search indices of word index
             bool complete = false;
-            string[] grid = new string[_gridSize];
-
-
             List<int> wordListIndex = new List<int>();
             wordListIndex.Add(0);
+
+            Console.WriteLine("Starting grid generation...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             while (!complete)
             {
                 if (wordListIndex[wordListIndex.Count - 1] >= _wordList.Count)
                 {
+                    if(wordListIndex.Count == 1)
+                    {
+                        throw new GridNotFoundException("No valid word grid found for provided characters");
+                    }
                     _wordGrid.RemoveAt(_wordGrid.Count - 1);
                     wordListIndex.RemoveAt(wordListIndex.Count - 1);
                     wordListIndex[wordListIndex.Count - 1] += 1;
@@ -75,10 +74,6 @@ namespace ConsoleApp1
                 else if (CheckValid(_wordList[wordListIndex[wordListIndex.Count - 1]]))
                 {
                     _wordGrid.Add(_wordList[wordListIndex[wordListIndex.Count - 1]]);
-                    if (_wordGrid[0] == "bravado")
-                    {
-
-                    }
                     wordListIndex.Add(0);
                 }
                 else
@@ -89,15 +84,14 @@ namespace ConsoleApp1
                 if (wordListIndex.Count > _gridSize)
                 {
                     wordListIndex.RemoveAt(wordListIndex.Count - 1);
-                    string compare = string.Empty;
-                    foreach (string word in _wordGrid)
-                    {
-                        compare = compare + word;
-                    }
+
+                    //Get character list from characters in valid grid
+                    string compare = string.Join(string.Empty,_wordGrid);
                     List<char> chars = compare.ToList();
                     chars.Sort();
                     compare = new string(chars.ToArray());
 
+                    //See if grid has valid characters
                     if (compare == _characters)
                     {
                         complete = true;
@@ -115,8 +109,24 @@ namespace ConsoleApp1
                     }
                 }
             }
+            sw.Stop();
+            _timeCompleted = sw.Elapsed;
+            Console.WriteLine("Grid Generation Completed");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
+        public List<string> GetWordGrid()
+        {
+            return _wordGrid;
+        }
+
+        public TimeSpan GetTimeCompleted()
+        {
+            return _timeCompleted;
+        }
+        #endregion
+
+        #region private members
         private bool CheckValid(string word)
         {
             for(int i = 0; i < _wordGrid.Count; i++)
@@ -139,6 +149,24 @@ namespace ConsoleApp1
                 }
             }
             return true;
+        }
+        #endregion
+    }
+
+    public class GridNotFoundException : Exception
+    {
+        public GridNotFoundException()
+        {
+        }
+
+        public GridNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public GridNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
         }
     }
 }
--- WordGrid/Program.cs
+++ WordGrid/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ConsoleApp1
+namespace WordGrid
 {
     class Program
     {
@@ -23,6 +23,25 @@ namespace ConsoleApp1
                     Console.WriteLine("Grid size is: {0} x {0}", gridSize);
                     Console.WriteLine("Available characters are: {0}", inputParts[1]);
                     CreateGrid wordGrid = new CreateGrid(gridSize, inputParts[1]);
+                    try
+                    {
+                        wordGrid.GenerateGrid();
+
+                        TimeSpan time = wordGrid.GetTimeCompleted();
+                        Console.WriteLine("Grid found in {0}:{1}:{2}", time.Minutes, time.Seconds, time.Milliseconds);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        foreach (string word in wordGrid.GetWordGrid())
+                        {
+                            Console.WriteLine(word);
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    catch(GridNotFoundException ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("EXCEPTION: {0}", ex.Message);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
                 }
                 else
                 {
